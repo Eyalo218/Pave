2bcf1e29c8d60203eb27c3fdd6cc50c1ad75913e
galJacob <joojonogal2@gmail.com>
2018-07-25 16:18:02 +0300

    commeneted router links

--- src/components/HomeComponents/Header.vue
+++ src/components/HomeComponents/Header.vue
@@ -3,9 +3,9 @@
       <div class="home-nav-bar">
         <div class="logo">Logo</div>
         <div class="links-container">
-          <router-link to="">How it works</router-link>
+          <!-- <router-link to="">How it works</router-link>
           <router-link>Sign up</router-link>
-          <router-link>Log in</router-link>
+          <router-link>Log in</router-link> -->
         </div>
       </div>
     </section>
