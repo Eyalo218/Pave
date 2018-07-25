d0079482851cdd25783015147a9aea4eb0395f54
galJacob <joojonogal2@gmail.com>
2018-07-25 16:47:40 +0300

    uploaded picture and added routes

--- src/components/HomeComponents/Header.vue
+++ src/components/HomeComponents/Header.vue
@@ -1,24 +1,41 @@
 <template>
     <section class="header">
-      <div class="home-nav-bar">
-        <div class="logo">Logo</div>
-        <div class="links-container">
-          <!-- <router-link to="">How it works</router-link>
-          <router-link>Sign up</router-link>
-          <router-link>Log in</router-link> -->
+      <div class="home-nav-bar flex space-between">
+        <div class="logo">Pave</div>
+        <div class="links-container flex space-between">
+          <router-link to="">How it works</router-link>
+          <router-link to="">Sign up</router-link>
+          <router-link to="">Log in</router-link>
         </div>
       </div>
     </section>
 </template>
 
 <script>
-
 export default {
-  name: 'HomeHeader',
+  name: "HomeHeader",
   components: {}
-}
+};
 </script>
+
 <style lang="scss">
-@import '../../../public/css/main.css';
+@import "../../../public/css/helpers.css";
+  .header{
+    margin: 0;
+    background-image: url("../../../public/img/home/homeImg.jpeg");
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+  }
+.home-nav-bar{
+  padding: 2rem;
+}
+.links-container{
+width: 20%;
+  a {
+    color: white;
+  }
+}
+
 
 </style>
