a3f2a878a64474921f38a3ca2b3822af5f52d232
Your Name <offirs12@gmail.com>
2018-07-25 17:00:29 +0300

    aa

--- src/components/HomeComponents/List.vue
+++ src/components/HomeComponents/List.vue
@@ -1,5 +1,15 @@
 <template>
     <section>
+        <div class="list-container">
+        <ul>
+            <li class="h-list-item">
+                <div class="item-img">1</div>
+                <div class="location">Red Deer, AL</div>
+                <div class="reviews">Reviews ****</div>
+                <div class="country">4</div>
+            </li>
+        </ul>
+        </div>
     </section>
 </template>
 
@@ -11,5 +21,23 @@ export default {
 }
 </script>
 <style lang="scss">
-    @import '../../../public/css/main.css'
+    .list-container {
+        font-family: 'roboto-medium';
+        color: #383633;
+        ul {
+            margin: 0;
+            padding: 0;
+        }
+        .h-list-item {
+            display: flex;
+            flex-direction: column;
+            .item-img {}
+            .location {
+                font-size: 0.8rem;
+            }
+            .country {
+                font-size: 0.8rem;
+            }
+        }
+    }
 </style>
