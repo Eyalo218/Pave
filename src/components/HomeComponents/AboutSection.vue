27c33a64af635fe5c2203ed5508347ec4b010d6f
Eyal Ohayon <EyalOhayon1@gmail.com>
2018-07-25 17:20:14 +0300

    finished aboutSection-cmp main structure

--- src/components/HomeComponents/AboutSection.vue
+++ src/components/HomeComponents/AboutSection.vue
@@ -1,48 +1,72 @@
 <template>
     <section>
-<<<<<<< HEAD
-        <div>
-            <div>
-                <h1>Plan faster, Easily</h1>
+        <div class="flex row-reverse space-between temp">
+            <div class="info">
+                <h1>Plan faster, easily</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas doloremque pariatur et aliquam</p>
             </div>
-            <div class="img"></div>
+            <div class="img img1"></div>
         </div>
-        <div>
-            <div>
-                <h1>Plan faster, Easily</h1>
+        <div class="flex space-between temp">
+            <div class="info">
+                <h1>Travel Orginaized</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas doloremque pariatur et aliquam</p>
             </div>
-            <div class="img"></div>
+            <div class="img img2"></div>
         </div>
-        <div>
-            <div>
-                <h1>Plan faster, Easily</h1>
+        <div class="flex row-reverse space-between temp">
+            <div class="info">
+                <h1>Follow and Lead</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas doloremque pariatur et aliquam</p>
             </div>
-            <div class="img"></div>
+            <div class="img img3"></div>
         </div>
-=======
->>>>>>> 750d1ec55bd63f54277c6f9b7765ed7b537a6563
     </section>
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {
-
-}
-</script>
-
-<style>
-
-=======
 
 export default {
   name: 'AboutSection',
   components: {}
 }
 </script>
+
 <style lang="scss">
->>>>>>> 750d1ec55bd63f54277c6f9b7765ed7b537a6563
+@import '../../../public/css/helpers.css';
+
+section{
+    width:95%;
+    margin: 0 auto;
+}
+
+.img{
+    width: 50%;
+    height: 185px;
+    background-position: center;
+}
+
+.img1{
+    background-image: url('../../../public/img/tempImg1.jpeg');
+}
+
+.img2{
+    background-position: left;
+    background-image: url('../../../public/img/tempImg2.jpeg');
+}
+
+.img3{
+    background-image: url('../../../public/img/tempImg3.jpeg');
+}
+
+.info{
+    width: 40%;
+}
+h1{
+    margin-bottom: 1.5rem;
+}
+.temp{
+    margin-bottom:2rem;
+
+}
 </style>
