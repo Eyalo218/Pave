f8d617d0504a0c208ee11feade4c62feedb09ac6
Eyal Ohayon <EyalOhayon1@gmail.com>
2018-07-25 16:06:40 +0300

    before pull

--- src/components/HomeComponents/AboutSection.vue
+++ src/components/HomeComponents/AboutSection.vue
@@ -1,1 +1,35 @@
- hbhb
+<template>
+    <section>
+        <div>
+            <div>
+                <h1>Plan faster, Easily</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas doloremque pariatur et aliquam</p>
+            </div>
+            <div class="img"></div>
+        </div>
+        <div>
+            <div>
+                <h1>Plan faster, Easily</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas doloremque pariatur et aliquam</p>
+            </div>
+            <div class="img"></div>
+        </div>
+        <div>
+            <div>
+                <h1>Plan faster, Easily</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas doloremque pariatur et aliquam</p>
+            </div>
+            <div class="img"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
